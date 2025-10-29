11379871da0869e46248a082d3be35a8ef3be788
Cesar Bermudez <cbermudez@unicolombo.edu.co>
2025-10-28 21:39:38 -0500

    Third commit

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -5,7 +5,16 @@ export class AppService {
   getHello() {
     return {
       msg: 'Hello world',
-      timestamp: new Date().toISOString(),
+      timestamp: this.getTime(),
     };
+  }
+
+  private getTime() {
+    const date = new Date();
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `${date.toISOString().slice(0, 'YYYY-MM-DD'.length)} ${hour}:${minutes}:${seconds}`;
   }
 }
